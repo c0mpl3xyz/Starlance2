ea9c6564db42a7d09ad2d3135e62a890cb009d8e
Altanbagana <altanbagana98@gmail.com>
2024-06-07 19:19:56 +0800

    fix

--- flask_app/sql_db/job.py
+++ flask_app/sql_db/job.py
@@ -190,7 +190,7 @@ class Job:
     def get_all_open_job(self):
         today = datetime.now(self.timezone)
         start_date = datetime(today.year, today.month, today.day)
-        query = "SELECT id FROM Job WHERE type = 'OPEN' and start_date <= %s"
+        query = "SELECT * FROM Job WHERE type = 'OPEN' and start_date <= %s"
         self.cursor.execute(query, (start_date,)) # type: ignore
         result = self.cursor.fetchall() # type: ignore
         return result
