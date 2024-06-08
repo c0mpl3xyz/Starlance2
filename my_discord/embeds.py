308f94a9d937a7fc3d8aea8575fb6742ab55a5bb
Altanbagana <altanbagana98@gmail.com>
2024-06-08 19:31:31 +0800

    fix

--- my_discord/embeds.py
+++ my_discord/embeds.py
@@ -43,6 +43,7 @@ class ContentEmbed(Embed):
         self.content_data = content_data
 
         color = None
+        print(f'{content_data=}')
         if content_data['active'] == 0 or not content_data['active']:
             color = discord.Color.red()
         else:
