88e07bf373df066f67d11a5b334849827bdae2a6
altnbgn <altanbagana@chimege.com>
2024-06-19 13:11:32 +0800

    fix

--- flask_app/app.py
+++ flask_app/app.py
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     app.run(
         # host='0.0.0.0', 
         # port=8000, 
-        ssl_context=('key/cert.pem', 'key/key.pem'), 
+        # ssl_context=('key/cert.pem', 'key/key.pem'), 
         threaded=True
     )
