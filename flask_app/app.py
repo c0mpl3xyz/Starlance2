d4ad953125141f3ad26ab27d3249b48967713849
altnbgn <altanbagana@chimege.com>
2024-06-21 03:10:48 +0800

    test

--- flask_app/app.py
+++ flask_app/app.py
@@ -48,7 +48,7 @@ def create_app():
     return app
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(content_updater, 'interval', seconds=5) 
+scheduler.add_job(content_updater, 'interval', seconds=30) 
 app = create_app()
 first = True
 
