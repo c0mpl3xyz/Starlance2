647b656a2764e89efd190b57ba24762eb03d0c7a
Altanbagana <altanbagana98@gmail.com>
2024-06-24 01:05:34 +0800

    fix

--- my_discord/usecases/get_company_jobs.py
+++ my_discord/usecases/get_company_jobs.py
@@ -29,6 +29,6 @@ class GetServerJobs:
         for job in JSON:
             response = requests.get(URL + '/content/job', json={'job_id': str(job[0])})
             contents = [content_mappings(content) for content in response.json()]
-            view = JobView(job_mapping(job), bot,  company=True, has_review=False, contents=contents)
+            view = JobView(job_mapping(job), bot,  company=True, has_review=False, contents=contents, server=True)
             job_views.append(view)
         return job_views
