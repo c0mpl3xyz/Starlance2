181ea3b45cb473cf673dc5bead1145c01c8d5df8
Altanbagana <altanbagana98@gmail.com>
2024-06-30 23:25:26 +0800

    fix

--- flask_app/enums.py
+++ flask_app/enums.py
@@ -1,3 +1,6 @@
 from enum import Enum
+import os
+
+HOME = os.path.expanduser('~')
 class ReportEnums(Enum):
-    JOB_REPORT_TEMPLATE = '~/Starlance2/flask_app/template/JOB_REPORT_TEMPLATE.docx'
+    JOB_REPORT_TEMPLATE = f'{HOME}/Starlance2/flask_app/template/JOB_REPORT_TEMPLATE.docx'
