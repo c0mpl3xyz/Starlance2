100f51f8dbdbcfdb80dc99b0edf47cb40d443600
Altanbagana <altanbagana@chimege.com>
2024-10-21 18:10:35 +0800

    fix

--- flask_app/app.py
+++ flask_app/app.py
@@ -96,7 +96,7 @@ def ig_login():
 if __name__ == '__main__':
     app.run(    
         # host='0.0.0.0', 
-        port=9000, 
+        port=8000, 
         # ssl_context=('key/cert.pem', 'key/key.pem'), 
         threaded=True
     )
--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ sudo systemctl restart ugc.service
 sudo systemctl enable ugc.service
 sudo systemctl restart ngrok.service
 sudo systemctl enable ngrok.service
-curl localhost:9000
+curl localhost:8000
 curl localhost:4040/api/tunnels
